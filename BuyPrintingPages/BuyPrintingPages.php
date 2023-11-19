b378e905735d0a3509f20e7cff5e226bcb8d17c4
Hà Thuỳ Dương <hathuyduong47@gmail.com>
2023-11-19 12:41:47 +0700

    Update Header & Footer

--- BuyPrintingPages/BuyPrintingPages.php
+++ BuyPrintingPages/BuyPrintingPages.php
@@ -53,16 +53,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dịch vụ sinh viên</title>
 
-    <!-- include header and footer files -->
-    <script async src="../footer.js"></script>
-    <script async src="../header.js"></script>  
+     
     <!-- custom css file link -->
-    <link rel="stylesheet" type="text/css" href="../BuyPrintingPages/BuyPrintingPages.css" >
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="BuyPrintingPages.css" >
 
 </head>
 <body>
     <!-- header section starts -->
-    <header id="app-header"></header>
+    <header id="app-header">
+        <section class="header">
+            <div class="left-side">
+                <div class="logo">
+                    <a href="#">
+                        <img src="../images/logo.png" alt="logo" />
+                        <p>ĐẠI HỌC QUỐC GIA TP.HCM<br>TRƯỜNG ĐẠI HỌC BÁCH KHOA</p>
+                    </a>
+                </div>
+                
+                <div class="menu-bar">
+                    <div class="first-option"><a href="">trang chủ</a></div>
+                    <div class="second-option"><a href="" >dịch vụ của tôi</a></div>
+                </div>
+            </div>
+        
+            <div class="right-side">
+                <div class="username">Username</div>
+                <div class="seperator">|</div>
+                <div>
+                    <a href="#" class="login">Đăng xuất</a>
+                </div>
+            </div>
+        </section>
+    </header>
     <!-- header section ends -->
 
 
@@ -176,7 +199,35 @@
 
 
     <!-- footer section starts -->
-    <footer id="app-footer"></footer>
+    <footer id="app-footer">
+        <div class="footer-container">
+            <section class="footer">
+                <div class="box-container">
+                    <div class="box">
+                        <h3>student smart printing service</h3>
+                        <img src="../images/logo.png" alt="logo" />
+                    </div>
+    
+                    <div class="box">
+                        <h3>website</h3>
+                        <a href="https://hcmut.edu.vn/" class="hcmut">HCMUT</a>
+                        <a href="https://mybk.hcmut.edu.vn/my/index.action" class="mybk">MyBK</a>
+                        <a href="https://mybk.hcmut.edu.vn/bksi/public/vi/" class="bksi">BKSI</a>
+                    </div>
+    
+                    <div class="box">
+                        <h3>liên hệ</h3>
+                        <a href="#"> <div class="location-icon"></div>268 Ly Thuong Kiet Street Ward 14, District 10, Ho Chi Minh City, Vietnam </a>
+                        <a href="#"> <div class="phone-icon"></div>(028) 38 651 670 - (028) 38 647 256 (Ext: 5258, 5234) </a>
+                        <a href="mailto:elearning@hcmut.edu.vn" class="email"> <div class="email-icon"></div>elearning@hcmut.edu.vn </a>
+                    </div>
+                </div>
+            </section>
+            <div class="copyright">
+                <p>Copyright 2007-2022 BKEL - Phát triển dựa trên Moodle</p>
+            </div>
+        </div>
+    </footer>
     <!-- footer section ends -->
 </body>
 </html>
