99d53be44de013e5eb6973fcd218234b4bd20d97
hienlq16103 <hienlq16103@gmail.com>
2023-11-19 09:45:25 +0700

    Implement configuration validation for configure system module

--- ConfigureSystem/ConfigureValidation.js
+++ ConfigureSystem/ConfigureValidation.js
@@ -1,3 +1,36 @@
 function ValidateConfiguration(){
-    
+    let numberOfPagesInput = document.querySelector("input[name='numberOfPages']");
+    let numberOfPages = Number(numberOfPagesInput.value);
+    if ((Number.isInteger(numberOfPages) == false) || (numberOfPages < 0)){
+        window.alert("Số trang phải là số nguyên dương\n");
+        return false;
+    }
+    window.alert("Cập nhật thành công\n");
+}
+function AddExtension(){
+    var extension = document.querySelector("input[name='extension']");
+    if (IsExtensionValid(extension) == false){
+        return;
+    }
+    extension.value = "";
+    window.alert("Thêm định dạng thành công\n");
+}
+function RemoveExtension(){
+    var extension = document.querySelector("input[name='extension']");
+    if (IsExtensionValid(extension) == false){
+        return;
+    }
+    extension.value = "";
+    window.alert("Xóa định dạng thành công\n");
+}
+function IsExtensionValid(extension){
+    if (extension.value == ""){
+        window.alert("Vui lập nhập tên extension\n");
+        return false;
+    }
+    if (extension.value.indexOf(".") != 0){
+        window.alert("Tên extension không hợp lệ\n");
+        return false;
+    }
+    return true;
 }
