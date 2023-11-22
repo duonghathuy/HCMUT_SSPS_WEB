a977139b71b670d88103714f4bcc664be2d1bc49
Hà Thuỳ Dương <hathuyduong47@gmail.com>
2023-11-23 03:37:24 +0700

    Update BuyPrintingPages.js

--- BuyPrintingPages/BuyPrintingPages.js
+++ BuyPrintingPages/BuyPrintingPages.js
@@ -1,11 +1,15 @@
-function openPopup(id) {
-    let popupNode = document.querySelector(id);
+function validateInputs() {
+    let quantityInput = document.getElementById('quantity');
+    let quantityValue = quantityInput.value;
 
-    popupNode.classList.add("active");
+    if(String(quantityValue).trim().length == 0) {
+        window.alert('Số lượng trang in cần mua thêm không hợp lệ!')
+        return false;
+    }
+
+    window.alert('Đăng kí thành công!')
 }
 
-function closePopup(id) {
-    let popupNode = document.querySelector(id);
-
-    popupNode.classList.remove("active");
+function confirmDelete() {
+    return window.confirm('Bạn muốn xóa đơn đăng kí mua trang in này?')
 }
