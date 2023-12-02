d72c43498049f6de530d9ce78d7a37f1964f25e8
Hà Thuỳ Dương <hathuyduong47@gmail.com>
2023-12-02 11:17:52 +0700

    Update DB

--- HCMUT_SSPS.sql
+++ HCMUT_SSPS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 01, 2023 at 08:17 PM
+-- Generation Time: Dec 02, 2023 at 05:16 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,11 +40,11 @@ CREATE TABLE `BPP_Order` (
 --
 
 INSERT INTO `BPP_Order` (`Order_ID`, `Order_Creation_Date`, `Quantity`, `Payment_Status`, `Owner_ID`) VALUES
-(44, '2023-11-09 13:57:32', 12, 1, 1),
-(45, '2023-11-09 13:57:37', 42, 1, 1),
-(63, '2023-11-19 20:22:33', 24, 1, 1),
-(110, '2023-11-25 18:02:17', 6, 0, 1),
-(111, '2023-11-25 18:03:52', 5, 0, 1);
+(44, '2023-11-09 13:57:32', 12, 1, 2110103),
+(45, '2023-11-09 13:57:37', 42, 1, 2110103),
+(63, '2023-11-19 20:22:33', 24, 1, 2110103),
+(110, '2023-11-25 18:02:17', 6, 0, 2110103),
+(111, '2023-11-25 18:03:52', 5, 0, 2110103);
 
 -- --------------------------------------------------------
 
@@ -110,10 +110,10 @@ CREATE TABLE `Printing_Request` (
 --
 
 INSERT INTO `Printing_Request` (`Request_ID`, `Registration_Date`, `Completion_Date`, `File_Name`, `Pages_Per_Sheet`, `Number_Of_Copies`, `Printer_ID`, `Request_Status`, `Owner_ID`) VALUES
-(1, '2023-10-09 07:05:29', '2023-10-09 10:18:02', '03_Ch3 Introduction_2023.pdf', 1, 4, 'Printer1', 'Đã hoàn thành', 1),
-(3, '2023-11-23 07:20:13', NULL, '02_Ch2 Introduction_2023.pdf', 4, 2, 'Printer2', 'Đã gửi', 1),
-(9, '2023-11-25 11:39:43', NULL, '01_Ch1 Introduction_2023.pdf', 2, 1, 'Printer2', 'Đã lưu', 1),
-(10, '2023-12-01 14:50:51', '2023-12-02 10:18:02', 'LAB 11.odt', 1, 2, 'Printer1', 'Đã hoàn thành', 3);
+(1, '2023-10-09 07:05:29', '2023-10-09 10:18:02', '03_Ch3 Introduction_2023.pdf', 1, 4, 'Printer1', 'Đã hoàn thành', 2110103),
+(3, '2023-11-23 07:20:13', NULL, '02_Ch2 Introduction_2023.pdf', 4, 2, 'Printer2', 'Đã gửi', 2110103),
+(9, '2023-11-25 11:39:43', NULL, '01_Ch1 Introduction_2023.pdf', 2, 1, 'Printer2', 'Đã lưu', 2110103),
+(10, '2023-12-01 14:50:51', '2023-12-02 10:18:02', 'LAB 11.odt', 1, 2, 'Printer1', 'Đã hoàn thành', 2110234);
 
 -- --------------------------------------------------------
 
@@ -162,27 +162,52 @@ INSERT INTO `Request_Status` (`Request_Status`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Student`
+-- Table structure for table `Users`
 --
 
-CREATE TABLE `Student` (
-  `Student_ID` int(11) NOT NULL,
+CREATE TABLE `Users` (
+  `ID` int(11) NOT NULL,
   `Fname` varchar(50) NOT NULL,
   `Lname` varchar(50) NOT NULL,
   `Email` text DEFAULT NULL,
   `Role` varchar(50) NOT NULL DEFAULT 'Student',
   `Sex` tinyint(4) DEFAULT NULL,
   `Balance` int(11) NOT NULL DEFAULT 0,
-  `DateOfBirth` date DEFAULT NULL
+  `DateOfBirth` date DEFAULT NULL,
+  `Username` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `Student`
+-- Dumping data for table `Users`
 --
 
-INSERT INTO `Student` (`Student_ID`, `Fname`, `Lname`, `Email`, `Role`, `Sex`, `Balance`, `DateOfBirth`) VALUES
-(1, 'Dương', 'Hà Thuỳ', 'duong.hathuy@hcmut.edu.vn', 'Student', 0, 50, '2003-12-13'),
-(3, 'Hoàng', 'Nguyễn Việt', 'hoang.nguyenviet@hcmut.edu.vn', 'Student', 1, 100, '2003-12-14');
+INSERT INTO `Users` (`ID`, `Fname`, `Lname`, `Email`, `Role`, `Sex`, `Balance`, `DateOfBirth`, `Username`) VALUES
+(2110103, 'Dương', 'Hà Thuỳ', 'duong.hathuy@hcmut.edu.vn', 'Student', 0, 100, '2023-02-15', 'duong.hathuy'),
+(2110234, 'Hoàng', 'Nguyễn Việt', 'hoang.nguyenviet@hcmut.edu.vn', 'Student', 1, 50, '2003-05-14', 'hoang.nguyenviet');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User_Addresses`
+--
+
+CREATE TABLE `User_Addresses` (
+  `User_ID` int(11) NOT NULL,
+  `Province` varchar(50) NOT NULL,
+  `District` varchar(50) NOT NULL,
+  `Commune` varchar(50) NOT NULL,
+  `Street` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `User_Addresses`
+--
+
+INSERT INTO `User_Addresses` (`User_ID`, `Province`, `District`, `Commune`, `Street`) VALUES
+(2110103, 'Thành Phố Hồ Chí Minh', 'Quận 3', 'Phường 7', '280 Điện Biên Phủ'),
+(2110103, 'Thành Phố Hồ Chí Minh', 'Quận 3', 'Phường 7', '58 Bà Huyện Thanh Quan'),
+(2110103, 'Tỉnh Long An', 'Huyện Cần Giuộc', 'Xã Thuận Thành', '88 Thuận Tây'),
+(2110234, 'Thành Phố Hồ Chí Minh', 'Quận 7', 'Phường Bình Thuận', '3 Lâm Văn Bền');
 
 --
 -- Indexes for dumped tables
@@ -229,10 +254,16 @@ ALTER TABLE `Request_Status`
   ADD PRIMARY KEY (`Request_Status`);
 
 --
--- Indexes for table `Student`
+-- Indexes for table `Users`
 --
-ALTER TABLE `Student`
-  ADD PRIMARY KEY (`Student_ID`);
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `User_Addresses`
+--
+ALTER TABLE `User_Addresses`
+  ADD PRIMARY KEY (`User_ID`,`Province`,`District`,`Commune`,`Street`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -251,10 +282,10 @@ ALTER TABLE `Printing_Request`
   MODIFY `Request_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `Student`
+-- AUTO_INCREMENT for table `Users`
 --
-ALTER TABLE `Student`
-  MODIFY `Student_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `Users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2110235;
 
 --
 -- Constraints for dumped tables
@@ -264,7 +295,7 @@ ALTER TABLE `Student`
 -- Constraints for table `BPP_Order`
 --
 ALTER TABLE `BPP_Order`
-  ADD CONSTRAINT `bpp_order_ibfk_1` FOREIGN KEY (`Owner_ID`) REFERENCES `student` (`Student_ID`);
+  ADD CONSTRAINT `bpp_order_ibfk_1` FOREIGN KEY (`Owner_ID`) REFERENCES `users` (`ID`);
 
 --
 -- Constraints for table `Printing_Request`
@@ -272,7 +303,13 @@ ALTER TABLE `BPP_Order`
 ALTER TABLE `Printing_Request`
   ADD CONSTRAINT `printing_request_ibfk_1` FOREIGN KEY (`Printer_ID`) REFERENCES `Printer` (`Printer_ID`),
   ADD CONSTRAINT `printing_request_ibfk_2` FOREIGN KEY (`Request_Status`) REFERENCES `Request_Status` (`Request_Status`),
-  ADD CONSTRAINT `printing_request_ibfk_3` FOREIGN KEY (`Owner_ID`) REFERENCES `Student` (`Student_ID`);
+  ADD CONSTRAINT `printing_request_ibfk_3` FOREIGN KEY (`Owner_ID`) REFERENCES `users` (`ID`);
+
+--
+-- Constraints for table `User_Addresses`
+--
+ALTER TABLE `User_Addresses`
+  ADD CONSTRAINT `user_addresses_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `Users` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
